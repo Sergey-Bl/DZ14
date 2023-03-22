f66bd7cfce32403b94fef068b238c85971e0ef1b
Sergei Blockiy <sergei.blockiy@gmail.com>
2023-03-22 13:09:06 +0200

    add descr

--- Assets/Script/Task1.cs
+++ Assets/Script/Task1.cs
@@ -8,6 +8,10 @@ public class Task1 : MonoBehaviour
 {
     private static async Task EnterText()
     {
+        // {Это метод EnterText(), который является асинхронным методом. Мы используем ключевое слово async, чтобы обозначить, 
+        //     что метод асинхронный. Затем мы используем ключевое слово await, чтобы дождаться выполнения асинхронной операции 
+        //     чтения содержимого файла textTask1.txt. Мы используем статический метод ReadAllTextAsync() класса File,
+        //         чтобы асинхронно прочитать содержимое файла в строку file
         var file = await File.ReadAllTextAsync(@"Assets/File/textTask1.txt");
         Debug.Log(file);
     }
